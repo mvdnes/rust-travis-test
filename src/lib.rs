e172a34e5a7d6053d2aa26697c5afec6ff4405a9
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-07-08 08:51:48 +0200

    Add some code for coverage testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
-#[test]
-fn it_works() {
+pub struct Foo {
+    a: u64,
+    b: i8,
+}
+
+impl Foo {
+    pub fn new() -> Foo {
+        Foo { a: 42, b: 41 }
+    }
+
+    pub fn add(&mut self) {
+        self.b += 1;
+    }
+
+    pub fn dec(&mut self) {
+        self.b -= 1;
+    }
+
+    pub fn get(&self) -> u64 {
+        self.a
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add() {
+        let mut f = Foo::new();
+        f.add();
+        assert_eq!(f.b, 42);
+    }
 }
