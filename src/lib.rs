36f322ebfb0cc81cb5c1a9f5664e9819e042eb01
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-07-11 09:43:47 +0200

    Add bzip2 for testing mingw compiler

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+extern crate bzip2;
+
+#[test]
+fn bzip2test() {
+    use std::io::prelude::*;
+    use bzip2::Compress;
+    use bzip2::reader::{BzCompressor, BzDecompressor};
+
+    // Round trip some bytes from a byte source, into a compressor, into a
+    // decompressor, and finally into a vector.
+    let data = "Hello, World!".as_bytes();
+    let compressor = BzCompressor::new(data, Compress::Best);
+    let mut decompressor = BzDecompressor::new(compressor);
+
+    let mut contents = String::new();
+    decompressor.read_to_string(&mut contents).unwrap();
+    assert_eq!(contents, "Hello, World!");
+}
+
 pub struct Foo {
     a: u64,
     b: i8,
