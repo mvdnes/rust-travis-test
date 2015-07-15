1508afad8813aac06cdf1193fb76d41e0b26228d
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-07-15 16:09:11 +0200

    Alternative kcov release and build with -g

--- script/kcov.sh
+++ script/kcov.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
 set -e
 shopt -s nullglob
 
 PROJECT=mytest
 TARGET=$HOME/kcov
+KCOV_RELEASE=v27
 
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
-tar xzf master.tar.gz
-mkdir kcov-master/build
-cd kcov-master/build
+wget https://github.com/SimonKagstrom/kcov/archive/$KCOV_RELEASE.tar.gz
+tar xzf $KCOV_RELEASE.tar.gz
+cd kcov-*
+mkdir build
+cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$TARGET
 make
 make install
@@ -16,14 +20,14 @@ export PATH=$TARGET/bin:$PATH
 
 #actual coverage testing:
 
-cargo rustc -- --test -C 'link-args=-Wl,--no-gc-sections'
+cargo rustc -- --test -C 'link-args=-Wl,--no-gc-sections' -g
 kcov --exclude-pattern=/.cargo target/kcov-lib target/debug/$PROJECT
 
 for d in tests/*; do
     basename=$(basename $d);
     testname=${basename%.*};
 
-    cargo rustc --test $testname -- -C 'link-args=-Wl,--no-gc-sections'
+    cargo rustc --test $testname -- -C 'link-args=-Wl,--no-gc-sections' -g
     kcov --exclude-pattern=/.cargo target/kcov-$testname target/debug/$testname-*
 done
 
